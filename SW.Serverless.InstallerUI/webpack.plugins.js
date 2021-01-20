f8f66a240448bacb4d48ecdc79e21f950608d975
shaheen-s <shaheensarafa17@gmail.com>
2021-01-20 12:31:12 +0200

    Removed binary build necessity

--- SW.Serverless.InstallerUI/webpack.plugins.js
+++ SW.Serverless.InstallerUI/webpack.plugins.js
@@ -2,10 +2,5 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = [
-  new CopyWebpackPlugin({
-					patterns: [
-									{from: "./binaries", to: "binaries"}
-					]
-	}),
   new ForkTsCheckerWebpackPlugin()
 ];
