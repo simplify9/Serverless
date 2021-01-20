9eddd142697195db06c1aa192c3d7d779faa9598
shaheen-s <shaheensarafa17@gmail.com>
2021-01-20 12:26:25 +0200

    Base port finalized

--- SW.Serverless.InstallerUI/src/installer-cli-port.ts
+++ SW.Serverless.InstallerUI/src/installer-cli-port.ts
@@ -1,13 +1,15 @@
 import {exec} from "child_process"
+import aws from "aws-sdk"
 import fs from "fs"
 import path from "path"
 import archiver from "archiver"
 import {Connection} from "./model"
 
+
 const buildPublish = (projectPath: string, outputPath: string, callback: (outPath: string, zipPath: string) => void ) => {
   exec(`dotnet publish "${projectPath}" -o "${outputPath}"`, (err, stdout, stderr) => {
     if(!err){
-      callback(outputPath, path.join(__dirname, "../build.zip"));
+      callback(outputPath, path.join(__dirname, "../tmp/build.zip"));
     }
     else {
       console.log(err);
@@ -37,8 +39,47 @@ const compress = (path: string, zipFileName: string, callback: (zipPath: string)
   } )
 }
 
-const pushToCloud = (zipPath: string, adapterId: string, entryAssembly: string, connection: Connection ) => {
+const pushToCloud = (zipPath: string, adapterId: string, entryAssembly: string, connection: Connection, callback: (err: string) => void ) => {
+  const s3 = new aws.S3({
+    accessKeyId: connection.accessKey,
+    endpoint: connection.endpoint,
+    secretAccessKey: connection.secretKey
+  });
 
+  s3.upload({
+    Bucket: connection.bucket,
+    ContentType: "application/zip",
+    Key: `adapters/${adapterId.toLowerCase()}`,
+    Body: fs.readFileSync(zipPath),
+    Metadata: {
+      "EntryAssembly": entryAssembly,
+      "Lang": 'dotnet'
+    }
+  }, {}, (err, data) => {
+    callback(err.message);
+  })
+}
+
+const getEntryAssembly = (adapterPath: string) => {
+  const fileName = path.parse(adapterPath).name;
+  return `${fileName}.dll`
+}
+
+const cleanup = (cleanUpPath: string) => {
+  fs.rmSync(cleanUpPath);
+}
+
+export default (adapterPath: string, adapterId: string, connection: Connection, callback: (result: string, isError: boolean) => void) => {
+  const tmpPath = path.join(__dirname, "../tmp/build");
+  buildPublish(adapterPath, tmpPath, (outPath, zipPath) => {
+    compress(outPath, zipPath, (zipPath) => {
+      pushToCloud(zipPath, adapterId, getEntryAssembly(adapterPath), connection, (err) => {
+        cleanup(path.join(__dirname, "../tmp"));
+        const result = err? err : `Successfully installer ${adapterId}`;
+        callback(result, err? true : false);
+      })
+    })
+  } )
 }
 
 
